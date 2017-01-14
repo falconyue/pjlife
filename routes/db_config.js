b1065073e1912665b3171cdcaaff90d0391e9ede
falconyue <1831180200@qq.com>
2017-01-14 14:16:27 +0800

    ...

--- routes/db_config.js
+++ routes/db_config.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 //填写数据库连接信息，可查询数据库详情页
 var username = 'userLTL';//用户AK
 var password = 'Fy8LmKCCaMchQN3F';//用户SK
-var db_host = process.env.OPENSHIFT_MYSQL_DB_HOST;
-var db_port = 3306;
+var db_host = process.env.MYSQL_SERVICE_HOST;
+var db_port = process.env.MYSQL_SERVICE_HOST;
 var db_name = 'sampledb';
 var option = {
   host: db_host,
@@ -12,7 +12,7 @@ var option = {
   password: password,
   database: db_name
 }
-console.log(process.env.OPENSHIFT_MYSQL_DB_USERNAME);
+
 var client = mysql.createConnection(option);
 
 module.exports = client;
