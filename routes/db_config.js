c958fbd6e0ae4048f8ef47666c099c76da7f3f9f
yuexiaoyu <falcon_dl@qq.com>
2017-01-18 16:40:41 +0800

    ...

--- routes/db_config.js
+++ routes/db_config.js
@@ -13,6 +13,19 @@ var option = {
   database: db_name
 }
 
-var client = mysql.createConnection(option);
+var pool = mysql.createPool(option);
 
-module.exports = client;
+exports.query = function(sql,callback){
+    pool.getConnection(function(err,conn){
+        if(err){
+            callback(err,null,null);
+        }else{
+            conn.query(sql,function(qerr,vals,fields){
+                //释放连接
+                conn.release();
+                //事件驱动回调
+                callback(qerr,vals,fields);
+            });
+        }
+    });
+};
